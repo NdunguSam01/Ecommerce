7257ff78393f5865722914d5f59a659fced4556f
NdunguMuigai <muigaisam65@gmail.com>
2022-03-24 16:39:48 +0300

    New line

--- cart.php
+++ cart.php
@@ -1,5 +1,5 @@
 <?php
-echo "Welcome to this page! Nothing has been coded yet!";
-echo  "<a href='/Ecommerce'>Please go back!</a> ";
-echo "No one is supposed to be here";
+echo "Welcome to this page! Nothing has been coded yet!<br><br>";
+echo  "<a href='/Ecommerce'>Please go back!</a><br><br>";
+echo "No one is supposed to be here ";
 ?>
